d333f8cc651e67ffcda750dccfce28b16eaf9352
Arthur_Jong <1115883977@qq.com>
2018-01-02 23:08:16 +0800

    first commit for testing github

---
+++ src/com/binfo/chapter01/practice01/solution01.java
@@ -0,0 +1,13 @@
+package com.binfo.chapter01.practice01;
+/**
+ * 
+ * @author Arthur
+ * @time 2018-01-02
+ * @description:给出使用欧几里得算法计算105和24的最大公约数的过程中得到的一系列p和q值。
+ * 		扩展该算法中的代码得到一个程序Euclid，从命令行接受两个参数，计算它们的最大公约数
+ * 		并打印出每次调用递归方法的两个参数。使用你的程序计算1 111 111和1 234 567的最大公约数。
+ * 
+ */
+public class solution01 {
+	
+}
